7d0f5b31b1ca58059e38eccef890d20930d2913f
MarionFoto <marionacp@gmail.com>
2017-05-04 11:05:38 -0300

    subiendo archivos

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function Pokemon(nombre,color,nivelDeAmistad,vida,poderDeAtaque){
+	this.nombre = nombre;
+	this.color = color;
+	this.nivelDeAmistad = nivelDeAmistad;
+	this.vida = vida;
+	this.poderDeAtaque = poderDeAtaque;
+	this.mostrarPokemon = function(){
+		return('Hola, soy: '+ this.nombre + 'y soy de color: ' + this.color)
+	}
+	this.aumentarAmistad = function(valor){
+		this.nivelDeAmistad = this.nivelDeAmistad + valor;
+	}
+	this.atacar = function (pokemon) {
+		pokemon.vida = pokemon.vida - this.poderDeAtaque
+	}
+
+}
+var pikachu = new Pokemon("Pikachu","rojo",0,8,2);
+var charmander = new Pokemon("Charmander","rojo",0,7,9);
+pikachu.atacar(charmander)
+console.log(charmander.vida)
