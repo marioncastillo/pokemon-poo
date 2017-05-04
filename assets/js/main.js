d62af86dfc1070204bdf2b38bbac5768efcd68b4
MarionFoto <marionacp@gmail.com>
2017-05-04 15:25:33 -0300

    agregue no pueden pelear

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,11 +23,17 @@ function Pokemon(nombre,color,nivelDeAmistad,vida,poderDeAtaque){
 
 function pelear(){
 
-var uno = document.getElementById("opcion1").value;
-var dos = document.getElementById("opcion2").value;
+var uno = document.getElementById("pok1").value;
+var dos = document.getElementById("pok2").value;
 var pokemon1 = new Pokemon(uno,"amarillo",100,30,60);
 var pokemon2 = new Pokemon(dos,"rojo",20,40,60);
+
+if(pokemon1.nombre == pokemon2.nombre){
+	alert("no pueden pelear")
+}
+
 pokemon1.atacar(pokemon2);
+
 
 	var resultado = document.getElementById("mostrar");
    var muestra = (pokemon1.nombre + " atacó a " + pokemon2.nombre + ". Y " + pokemon2.nombre + " tiene una vida de " + pokemon2.vida);
